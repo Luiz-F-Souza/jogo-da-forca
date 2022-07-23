beb8f7b44137024a65ac3a63653f56a7ff08c892
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2022-07-23 02:38:00 -0300

    Havia feito o jogo funcional com interface,  mas acabei deixando sem a validação solicitada. O item foi add nesse commit

--- desafio/desafio.js
+++ desafio/desafio.js
@@ -1,14 +1,27 @@
 
-
 const readline = require('readline-sync');
 const Forca = require('./forca');
 
+
+
 const jogo = new Forca('abacaxi');
+
+jogo.mascarandoPalavra()
 
 while (!["perdeu", "ganhou"].includes(jogo.buscarEstado())) {
     const chute = readline.question("Aguardando chute: \n");
+    
     jogo.chutar(chute);
     console.log(jogo.buscarDadosDoJogo());
 }
 
 console.log("você " + jogo.buscarEstado());
+
+
+// 1  cria uma new forca com a palavra desejada
+// 2 verifica se há 'perdeu' ou 'ganhou' em jogo.buscarEstado()
+// 3 pergunta qual o chute
+// 4 fornece o chute digitado a jogo.chutar
+// 5 mostra os dados contidos em jogo.buscar dados do jogo
+// 6 pega o return de jogo.buscarEstado() e da como frase se ganhou ou perdeu
+
